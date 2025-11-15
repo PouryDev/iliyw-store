5e47f50b9ef8dfc445f3d82fe3fbe6e446c48811
PK <p.khazaei@snappgrocery.com>
2025-11-15 18:16:24 +0330

    enamad

--- resources/views/react-app.blade.php
+++ resources/views/react-app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="enamad" content="15119651" />
 
-    <title>{{ config('app.name', 'جمه') }}</title>
+    <title>15119651</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
