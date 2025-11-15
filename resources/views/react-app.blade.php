2d9a39d0d4977edea8f1fcc6959a838f91421032
PK <p.khazaei@snappgrocery.com>
2025-11-15 18:39:32 +0330

    revert enamad changes on the main react-app file

--- resources/views/react-app.blade.php
+++ resources/views/react-app.blade.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="enamad" content="15119651" />
 
-    <title>15119651</title>
+    <title>{{ config('app.name', 'جمه') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
